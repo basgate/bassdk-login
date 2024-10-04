a2f7b9ae3c0a01a0a8ba03e8690eeb33f388924b
Abdullah AlAnsi <aalansi@yk-bank.com>
2024-10-04 05:00:59 +0300

    Add CDN script and update login form with authentication logic

--- bassdk-wp-login.php
+++ bassdk-wp-login.php
@@ -11,6 +11,7 @@ function bassdk_enqueue_scripts()
     wp_enqueue_style('bassdk-login-styles', plugin_dir_url(__FILE__) . 'css/styles.css', array(), '1.0');
     //TODO: Replace this script with cdn url
     wp_enqueue_script('bassdk-login-script', plugin_dir_url(__FILE__) . 'js/script.js', array('jquery'), '1.0', true);
+    wp_enqueue_script('bassdk-login-cdn-script', esc_url('https://pub-8bba29ca4a7a4024b100dca57bc15664.r2.dev/sdk/stage/v1/public.js'), array('jquery'), '1.0', true);
     //TODO: Add Admin options part
 }
 add_action('wp_enqueue_scripts', 'bassdk_enqueue_scripts');
@@ -23,6 +24,13 @@ function bassdk_login_form()
         <div class="bassdk-modal-content" style="text-align: center;">
             <span class="bassdk-close">&times;</span>
             <h2>BAS Login</h2>
+            <script type="text/javascript">
+                console.log("isJSBridgeReady :", isJSBridgeReady)
+                await getBasAuthCode("653ed1ff-59cb-41aa-8e7f-0dc5b885a024").then((res) => {
+                    console.log("Logined Successfully :", res)
+                    alert("Logined Successfully ")
+                })
+            </script>
             <form method="post" action="<?php echo esc_url(site_url('wp-login.php', 'login_post')); ?>">
                 <label for="username">Username:</label>
                 <input type="text" name="log" id="username" required>
--- js/script.js
+++ js/script.js
@@ -21,103 +21,103 @@ jQuery(document).ready(function ($) {
 });
 
 
-/////////// Basgate SDK for Login ///////////
+// /////////// Basgate SDK for Login ///////////
 
-//#region BAS SDK Client Side 
-var isJSBridgeReady = false
-console.log("Start Bas-ClientSDK Script");
+// //#region BAS SDK Client Side 
+// var isJSBridgeReady = false
+// console.log("Start Bas-ClientSDK Script");
 
-async function initBas() {
-    console.log("initBas() STARTED");
-    window.addEventListener("JSBridgeReady", async (event) => {
-        console.log("JSBridgeReady fired ");
-        isJSBridgeReady = true
-    }, false);
-}
+// async function initBas() {
+//     console.log("initBas() STARTED");
+//     window.addEventListener("JSBridgeReady", async (event) => {
+//         console.log("JSBridgeReady fired ");
+//         isJSBridgeReady = true
+//     }, false);
+// }
 
-initBas();
+// initBas();
 
-const isBasSupperApp = () => {
-    return isJSBridgeReady;
-}
+// const isBasSupperApp = () => {
+//     return isJSBridgeReady;
+// }
 
-const getBasAuthCode = async (clientId) => {
-    if (!isJSBridgeReady) await initBas();
-    console.log("getBasAuthCode() STARTED");
+// const getBasAuthCode = async (clientId) => {
+//     if (!isJSBridgeReady) await initBas();
+//     console.log("getBasAuthCode() STARTED");
 
-    return JSBridge.call('basFetchAuthCode',
-        {
-            clientId: clientId
-        }).then(function (result) {
-            /****** Response Example ******/
-            /*
-            {
-                "status":1,
-                "data":{
-                    "auth_id":"FD268ED889B7DFB008093D04809E8B7FC26B821421B278",
-                    "authid":"FD268ED889B7DFB008093D04809E8B7FC26B821421B278",
-                    "openid":"null",
-                    "return_url":"null"},
-                "messages":["تمت العملية بنجاح"]
-            }
-            */
-            /****** End Response Example ******/
-            // alert(JSON.stringify(result))
-            console.log("getBasAuthCode Result:", JSON.stringify(result));
-            if (result) {
-                return result;
-            } else {
-                return null
-            }
-        });
-}
+//     return JSBridge.call('basFetchAuthCode',
+//         {
+//             clientId: clientId
+//         }).then(function (result) {
+//             /****** Response Example ******/
+//             /*
+//             {
+//                 "status":1,
+//                 "data":{
+//                     "auth_id":"FD268ED889B7DFB008093D04809E8B7FC26B821421B278",
+//                     "authid":"FD268ED889B7DFB008093D04809E8B7FC26B821421B278",
+//                     "openid":"null",
+//                     "return_url":"null"},
+//                 "messages":["تمت العملية بنجاح"]
+//             }
+//             */
+//             /****** End Response Example ******/
+//             // alert(JSON.stringify(result))
+//             console.log("getBasAuthCode Result:", JSON.stringify(result));
+//             if (result) {
+//                 return result;
+//             } else {
+//                 return null
+//             }
+//         });
+// }
 
-const getBasPayment = async (data) => {
-    let paymentParams = {
-        "amount": {
-            "value": data.order?.amount?.totalAmount ?? '0',
-            "currency": data.order?.amount?.currency ?? 'YER',
-        },
-        "orderId": data.order?.orderId ?? '111',
-        "trxToken": data.trxToken,
-        // "merchantId": BAS_CLIENT_ID,
-        "appId": BAS_APP_ID
-    }
-    // let paymentParams = {
-    //     "amount": {
-    //         "value": data.order?.amount?.totalAmount ?? '0',
-    //         "currency": data.order?.amount?.currency ?? 'YER',
-    //     },
-    //     "orderId": data.order?.orderId ?? '111',
-    //     "transactionToken": data.trxToken,
-    //     "merchantId": BAS_CLIENT_ID,
-    //     "appId": BAS_APP_ID
-    // }
-    console.log("========== getBasPayment Params :", JSON.stringify(paymentParams))
-    return JSBridge.call('basPayment', paymentParams).then(function (result) {
-        /****** Response Example ******/
-        /*{
-        "merchantId": "",
-        "orderId": "",
-        "transactionId": "",
-        "amount": {
-        "value": 0,
-        "currency": "YER"
-        },
-        "paymentType": "",
-        "date": "",
-        "status":1
-        }*/
-        /****** End Response Example ******/
-        console.log("basPayment Result:", JSON.stringify(result));
-        if (result) {
-            return result;
-        } else {
-            return null
-        }
-    });
+// const getBasPayment = async (data) => {
+//     let paymentParams = {
+//         "amount": {
+//             "value": data.order?.amount?.totalAmount ?? '0',
+//             "currency": data.order?.amount?.currency ?? 'YER',
+//         },
+//         "orderId": data.order?.orderId ?? '111',
+//         "trxToken": data.trxToken,
+//         // "merchantId": BAS_CLIENT_ID,
+//         "appId": BAS_APP_ID
+//     }
+//     // let paymentParams = {
+//     //     "amount": {
+//     //         "value": data.order?.amount?.totalAmount ?? '0',
+//     //         "currency": data.order?.amount?.currency ?? 'YER',
+//     //     },
+//     //     "orderId": data.order?.orderId ?? '111',
+//     //     "transactionToken": data.trxToken,
+//     //     "merchantId": BAS_CLIENT_ID,
+//     //     "appId": BAS_APP_ID
+//     // }
+//     console.log("========== getBasPayment Params :", JSON.stringify(paymentParams))
+//     return JSBridge.call('basPayment', paymentParams).then(function (result) {
+//         /****** Response Example ******/
+//         /*{
+//         "merchantId": "",
+//         "orderId": "",
+//         "transactionId": "",
+//         "amount": {
+//         "value": 0,
+//         "currency": "YER"
+//         },
+//         "paymentType": "",
+//         "date": "",
+//         "status":1
+//         }*/
+//         /****** End Response Example ******/
+//         console.log("basPayment Result:", JSON.stringify(result));
+//         if (result) {
+//             return result;
+//         } else {
+//             return null
+//         }
+//     });
 
-}
+// }
 
 
 //#endregion
